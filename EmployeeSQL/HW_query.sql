abf384343e415ba78b9512d713599e5e2bb0c37c
Solomon Rotich <100892067+Srotich1@users.noreply.github.com>
2022-05-13 13:39:07 -0400

    hw_queries update

--- EmployeeSQL/HW_query.sql
+++ EmployeeSQL/HW_query.sql
@@ -1,82 +1,11 @@
--- -- create a table schema for each of the six CSV files.
--- * Import each CSV file into the corresponding SQL table.
--- -- Remember to specify data types, primary keys, foreign keys, and other constraints.
---1 ****department csv****
---"dept_no","dept_name"
-
-CREATE TABLE departments (
-	dept_no varchar primary key,
-	dept_name VARCHAR
-);
-
-select *
-from departments
-
---2 ****dept_emp.csv*****
---emp_no,dept_no
-CREATE TABLE dept_emp (
-	emp_no int,
-	dept_no varchar
-);
-
-select *
-from dept_emp
-
---3***dept_manager.csv
---dept_no,emp_no
-CREATE TABLE dept_manager (
-	dept_no varchar,
-	emp_no VARCHAR
-);
-
-select *
-from dept_manager
-
---4***employees.csv
---emp_no,emp_title_id,birth_date,first_name,last_name,sex,hire_date
-CREATE TABLE EMPLOYEES (
-	emp_no VARCHAR NOT NULL,
-	emp_title_id varchar NOT NULL,
-	birth_date date NOT NULL,
-	first_name varchar NOT NULL,
-	last_name varchar NOT NULL,
-	sex varchar,
-	hire_date DATE NOT NULL
-);
-
-select *
-from EMPLOYEES
-
---5***salaries.csv
---emp_no,salary
-CREATE TABLE SALARIES (
-	emp_no VARCHAR NOT NULL,
-	SALARY VARCHAR 
-);
-
-select *
-from SALARIES
-
---6****titles.csv
---title_id,title
-CREATE TABLE TITLES (
-	emp_title_id varchar NOT NULL,
-	TITLE VARCHAR 
-);
-
-select *
-from TITLES
+--  ****** Queries*******
 
 -- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
--- SELECT column_name(s)
--- FROM table1
--- LEFT JOIN table2
--- ON table1.column_name = table2.column_name;
+
 SELECT (LAST_NAME, FIRST_NAME,SEX,SALARY)
 FROM EMPLOYEES
 LEFT JOIN SALARIES
-ON EMPLOYEES.EMP_NO = SALARIES.salary;
-
+ON EMPLOYEES.EMP_NO = SALARIES.emp_no;
 
 -- 2. List first name, last name, and hire date for employees who were hired in 1986.
 SELECT (FIRST_NAME, LAST_NAME, HIRE_DATE)
@@ -127,18 +56,26 @@ LEFT JOIN employees
 ON dept_manager.emp_no = employees.emp_no
 LEFT JOIN departments
 ON dept_manager.dept_no = departments.dept_no
-
+where dept_name = 'Sales'
 
 -- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
-
-
+SELECT dept_manager.emp_no,
+employees.last_name, 
+employees.first_name,
+departments.dept_name
+FROM dept_manager
+LEFT JOIN employees 
+ON dept_manager.emp_no = employees.emp_no
+LEFT JOIN departments
+ON dept_manager.dept_no = departments.dept_no
+where dept_name = 'Sales' or dept_name ='Development'
 
 -- 8. List the frequency count of employee last names (i.e., how many employees share each last name) in descending order.
 
-
-
-
-
+select last_name, count(last_name)
+from employees
+group by last_name
+order by count(last_name) desc
 
 
 
